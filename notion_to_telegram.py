23629eaad620508d624d4296848e1a3b457e9c4b
Duden Heryana <63181967+dudenheryana1994@users.noreply.github.com>
2025-04-29 10:39:51 +0700

    Update notion_to_telegram.py

--- notion_to_telegram.py
+++ notion_to_telegram.py
@@ -4,6 +4,7 @@ import os
 from dotenv import load_dotenv
 from datetime import datetime
 import logging
+import sys
 
 # Konfigurasi logging
 logging.basicConfig(level=logging.DEBUG)
@@ -104,12 +105,12 @@ def extract_formula(prop):
 def main():
     notion_data = get_notion_data()
     if not notion_data:
-        return
+        sys.exit(0)
 
     results = notion_data.get("results", [])
     if not results:
         logger.info("No data found.")
-        return
+        sys.exit(0)
 
     sent_ids = read_sent_ids()
 
@@ -161,6 +162,9 @@ def main():
             send_to_telegram(tele_id_u, message)
             sent_ids.append(item_id)
             save_sent_ids(sent_ids)
+            
+    logger.info("Processing completed.")
+    sys.exit(0)
 
 if __name__ == "__main__":
     main()
